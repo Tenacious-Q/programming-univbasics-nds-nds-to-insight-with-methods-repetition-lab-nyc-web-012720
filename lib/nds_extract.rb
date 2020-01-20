a45e44de5f4ede8b72ac00d50d68adc5e4b6f0f0
Michael Quigley <mikequigley@quigleypublishing.com>
2020-01-20 21:52:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -58,6 +58,6 @@ puts directors_totals(directors_database)
 
 #puts directors_database[0][:name]
 
-puts list_of_directors(directors_database)
+puts list_of_directors(directors_totals(directors_database))
 
 #puts gross_for_director(directors_database)
